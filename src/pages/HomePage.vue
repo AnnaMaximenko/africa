672a5bb6f36863c74a6266d18238749665a67063
AnyaMaximenko <any-maksimenk@yandex.ru>
2024-04-08 13:01:44 +0300

    add header

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <Header />
+  <IndexHtml />
+  <Footer />
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import IndexHtml from "@/components/IndexHtml.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  components: {
+    Header,
+    IndexHtml,
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
