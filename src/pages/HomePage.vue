b0be80f036b3f7eb2f5730d048cf34feb447bcfd
AnyaMaximenko <any-maksimenk@yandex.ru>
2024-04-15 20:34:12 +0300

    add section-2,3,4,5

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,17 +1,29 @@
 <template>
   <Header />
-  <IndexHtml />
+  <Section1Html />
+  <Section2Html />
+  <Section3Html />
+  <Section4Html />
+  <Section5Html />
   <Footer />
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
-import IndexHtml from "@/components/IndexHtml.vue";
+import Section1Html from "@/components/Section1Html.vue";
+import Section2Html from "@/components/Section2Html.vue";
+import Section3Html from "@/components/Section3Html.vue";
+import Section4Html from "@/components/Section4Html.vue";
+import Section5Html from "@/components/Section5Html.vue";
 import Footer from "@/components/Footer.vue";
 export default {
   components: {
     Header,
-    IndexHtml,
+    Section1Html,
+    Section2Html,
+    Section3Html,
+    Section4Html,
+    Section5Html,
     Footer,
   },
 };
