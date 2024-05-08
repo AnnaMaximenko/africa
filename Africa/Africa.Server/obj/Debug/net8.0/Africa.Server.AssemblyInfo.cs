bf636a8a95fbc8cf0d6ee792ec43f12965fc863b
AnyaMaximenko <any-maksimenk@yandex.ru>
2024-05-08 20:50:40 +0300

    add footer

--- Africa/Africa.Server/obj/Debug/net8.0/Africa.Server.AssemblyInfo.cs
+++ Africa/Africa.Server/obj/Debug/net8.0/Africa.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Africa.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d75ee59e312088f2a6e76aae94aa37963263b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738b84a2da5966e4bfd3d1b7566dd83f6623b2ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Africa.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Africa.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
