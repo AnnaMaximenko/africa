45872229e2a3da7b48f6bda08067ed65dc47cdc5
AnyaMaximenko <any-maksimenk@yandex.ru>
2024-05-08 21:27:01 +0300

    remove conflict

--- Africa/Africa.Server/obj/Debug/net8.0/Africa.Server.AssemblyInfo.cs
+++ Africa/Africa.Server/obj/Debug/net8.0/Africa.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Africa.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+958e963e475f82c9e80df1a75d4efd86c60f1bd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad0e6ccb506f322ba6b3e3ae00e40c953f85353")]
 [assembly: System.Reflection.AssemblyProductAttribute("Africa.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Africa.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
