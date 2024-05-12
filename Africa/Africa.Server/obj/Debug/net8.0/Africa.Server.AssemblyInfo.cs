abc2653780388f7a611eaf7c4588f7ee22c2e67a
AnyaMaximenko <any-maksimenk@yandex.ru>
2024-05-12 09:42:07 +0300

    del node

--- Africa/Africa.Server/obj/Debug/net8.0/Africa.Server.AssemblyInfo.cs
+++ Africa/Africa.Server/obj/Debug/net8.0/Africa.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Africa.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5118f6dda379af2b56468e7637edd496085f447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ac5093bc7fa035715d74bdd53e79c9b9c8dfad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Africa.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Africa.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
