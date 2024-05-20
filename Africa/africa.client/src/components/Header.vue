3051da5bb6e2508945345a1ab2adf218b5fce397
lizau <liza.uchaneva@outlook.com>
2024-05-20 13:34:54 -0700

    added method onSendApplication

--- Africa/africa.client/src/components/Header.vue
+++ Africa/africa.client/src/components/Header.vue
@@ -1,110 +1,78 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <header>
-    <div id="app" class="header__wrapper center">
-      <a href="#">
-        <img src="../assets/img/Logo_Frika Safaris.svg" alt="logo"
-      /></a>
-      <div class="header-right__menu">
-        <router-link
-          class="header__menu"
-          v-for="link in rightArr"
-          :key="link.id"
-          :to="link.url"
-        >
-          {{ link.name }}
-        </router-link>
-        <div class="header__dropdown">
-          <button @click="show = !show" key="menu" class="header__button">
-            PLAN A TRIP
-          </button>
+    <header>
+        <div id="app" class="header__wrapper center">
+            <a href="#">
+                <img src="../assets/img/Logo_Frika Safaris.svg" alt="logo" />
+            </a>
+            <div class="header-right__menu">
+                <router-link class="header__menu"
+                             v-for="link in rightArr"
+                             :key="link.id"
+                             :to="link.url">
+                    {{ link.name }}
+                </router-link>
+                <div class="header__dropdown">
+                    <button @click="show = !show" key="menu" class="header__button">
+                        PLAN A TRIP
+                    </button>
 
-          <form
-            action="https://localhost:7272/api/Applications"
-            method="POST"
-            v-if="show"
-            id="myDropdown"
-            class="form"
-            @submit.prevent="send"
-          >
-            <h3 class="form__title">Application form</h3>
-            <input
-              placeholder="Name"
-              required
-              class="form__input"
-              type="text"
-              name="name"
-              id=""
-              v-model.trim="form.name"
-            />
-            <input
-              placeholder="Phone"
-              required
-              class="form__input"
-              type="tel"
-              name="phone"
-              id=""
-              v-model.trim="form.phone"
-            />
-            <input
-              placeholder="E-mail"
-              required
-              class="form__input"
-              type="email"
-              name=""
-              id=""
-              v-model.trim="form.email"
-            />
-            <button type="submit" class="form__button">Send</button>
-
-            <div class="reviews">
-              <ul>
-                <li v-for="item in form" :key="item.id">
-                  <p>{{ item.name }}</p>
-                </li>
-              </ul>
+                    <form @submit.prevent = "onSendApplication" v-if="show" id="myDropdown" class="form">
+                        <h3 class="form__title">Application form</h3>
+                        <input placeholder="Name"
+                               v-model ="applicationData.name"
+                               required
+                               class="form__input"
+                               type="text"
+                               name="name"
+                               id="" />
+                        <input placeholder="Phone"
+                               v-model ="applicationData.phone"
+                               required
+                               class="form__input"
+                               type="tel"
+                               name="phone"
+                               id="" />
+                        <input placeholder="E-mail"
+                               v-model ="applicationData.email"
+                               required
+                               class="form__input"
+                               type="email"
+                               name=""
+                               id="" />
+                        <button class="form__button">Send</button>
+                    </form>
+                </div>
             </div>
-          </form>
         </div>
-      </div>
-    </div>
-  </header>
+    </header>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      name: "",
-      phone: "",
-      email: "",
-      rightArr: [
-        { id: 0, name: "Destinations", url: "/" },
-        { id: 1, name: "About Us", url: "/" },
-        { id: 2, name: "Experience", url: "/" },
-        { id: 3, name: "Blog", url: "/" },
-      ],
-      show: false,
+    import axios from 'axios';
+    export default {
+        data() {
+            return {
+                applicationData:{
+                    name: '',
+                    phone: '',
+                    email: '',
+                },
+                rightArr: [
+                    { id: 0, name: "Destinations", url: "/" },
+                    { id: 1, name: "About Us", url: "/" },
+                    { id: 2, name: "Experience", url: "/" },
+                    { id: 3, name: "Blog", url: "/" },
+                ],
+                show: false,
+            };
+        },
+        methods: {
+            onSendApplication() {
+                return axios.post('https://localhost:7272/api/Applications', this.applicationData);
+            }
+        },
     };
-  },
-  methods: {
-    // send() {
-    //   axios.post(
-    //     "http://localhost/priangan/api/user/login",
-    //     {
-    //       name: this.state.name,
-    //       phone: this.state.phone,
-    //       email: this.state.email,
-    //     },
-    //     {
-    //       headers: {
-    //         "Content-type": "application/json; charset=UTF-8",
-    //       },
-    //     }
-    //   );
-    // },
-  },
-};
 </script>
 
 <style lang="scss" scoped></style>
