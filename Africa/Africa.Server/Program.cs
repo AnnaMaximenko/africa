a8cea46fbebc205f40288d102d543e4478fe95d4
lizau <liza.uchaneva@outlook.com>
2024-05-21 13:54:02 -0700

    Added Startup.cs and divided Program.cs

--- Africa/Africa.Server/Program.cs
+++ Africa/Africa.Server/Program.cs
@@ -1,44 +1,21 @@
 using Africa.Server.Data;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<ApplicationsContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationTable")));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Africa.Server
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseCors(builder =>
-{
-    builder
-        .WithOrigins("https://localhost:5173")
-        .AllowAnyMethod()
-        .AllowAnyHeader();
-});
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
-
-app.Run();
---
+++ Africa/Africa.Server/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Africa.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Africa.Server
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; protected set; }
+        public Startup(IWebHostEnvironment env)
+        {
+            this.Environment = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddSpaStaticFiles(options => { options.RootPath = "wwwroot"; });
+            services.AddControllers();
+
+            string conn = Configuration.GetConnectionString("ApplicationTable");
+            services.AddDbContext<ApplicationsContext>(opt => opt.UseSqlServer(conn));
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(builder =>
+            {
+                builder
+                    .WithOrigins("https://localhost:5173")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => { 
+                endpoints.MapControllers(); 
+                endpoints.MapFallbackToFile("/index.html"); 
+            });
+            //app.UseSpaStaticFiles();
+            //app.UseSpa(builder =>
+            //{
+            //    if (env.IsDevelopment())
+            //    {
+            //        builder.UseProxyToSpaDevelopmentServer("https://localhost:5173");
+            //    }
+            //});
+        }
+    }
+}
