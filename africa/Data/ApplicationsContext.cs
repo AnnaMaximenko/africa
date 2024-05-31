17021eeb0258da9b7654e8854ddb179a2b7f7235
lizau <liza.uchaneva@outlook.com>
2024-05-31 16:59:44 -0700

    added data and controller

---
+++ africa/Data/ApplicationsContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace africa.Data
+{
+    public class ApplicationsContext : DbContext
+    {
+        public ApplicationsContext(DbContextOptions<ApplicationsContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Application> Applications { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+    }
+}
