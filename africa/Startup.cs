f7bb66914ac5f979654c8328fee4a550ab7e12e5
lizau <liza.uchaneva@outlook.com>
2024-05-31 19:21:13 -0700

    deleted forecast

--- africa/Startup.cs
+++ africa/Startup.cs
@@ -1,17 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Builder.Extensions;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using VueCliMiddleware;
 
 public class Startup
