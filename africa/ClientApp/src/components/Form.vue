941ae0b6661deffd2fa0d0de2a5d02fd6df43705
lizau <liza.uchaneva@outlook.com>
2024-06-24 15:33:50 -0700

    added validation in form

--- africa/ClientApp/src/components/Form.vue
+++ africa/ClientApp/src/components/Form.vue
@@ -4,29 +4,47 @@
           id="myDropdown"
           class="form center">
         <h3 class="form__title">Application form</h3>
-        <div>
+        <div class="form__input_box">
             <label class="form__label">Name</label>
-            <input v-model="applicationData.name"
-                   class="form__input"
+            <input class="form__input"
+                   v-bind:class="{ 'form__input_hasError': v$.name.$invalid }"
+                   v-model="v$.name.$model"
                    type="text"
                    name="name"
                    id="" />
+            <p class="form__error_message"
+                  v-for="err in v$.name.$errors"
+                  :key="err.$uid">
+                *{{ err.$message }}
+            </p>
         </div>
-        <div>
+        <div class="form__input_box">
             <label class="form__label">Phone</label>
-            <input v-model="applicationData.phone"
+            <input v-model="v$.phone.$model"
+                   v-bind:class="{ 'form__input_hasError': v$.phone.$invalid }"
                    class="form__input"
                    type="tel"
                    name="phone"
                    id="" />
+            <p class="form__error_message"
+                  v-for="err in v$.phone.$errors"
+                  :key="err.$uid">
+                *{{ err.$message }}
+            </p>
         </div>
-        <div>
+        <div class="form__input_box">
             <label class="form__label">Email</label>
-            <input v-model.trim="applicationData.email"
+            <input v-model.trim="v$.email.$model"
+                   v-bind:class="{ 'form__input_hasError': v$.email.$invalid }"
                    class="form__input"
                    type="email"
                    name=""
                    id="" />
+            <p class="form__error_message"
+                  v-for="err in v$.email.$errors"
+                  :key="err.$uid">
+                *{{ err.$message }}
+            </p>
         </div>
         <button type="submit" class="form__button">Send</button>
     </form>
@@ -34,45 +52,50 @@
 
 <script>
     import axios from 'axios';
+    import { reactive, computed } from 'vue';
     import { email, required, helpers } from '@vuelidate/validators';
-    //import useVuelidate from '@vuelidate/core';
-
-    const number = helpers.regex(
-        "serial",
-        /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/
-    );
+    import { useVuelidate } from '@vuelidate/core';
 
     export default {
-        data() {
+        setup() {
+            const application = reactive({
+                name: '',
+                phone: '',
+                email: '',
+            });
+
+            const number = helpers.regex(
+                "serial",
+                /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/
+            );
+
+            const rules = computed(() => ({
+                name: { required },
+                email: { required, email },
+                phone: { required, number }
+            }));
+
+            const v$ = useVuelidate(rules, application);
+
             return {
-                applicationData: {
-                    name: '',
-                    phone: '',
-                    email: '',
-                }
+                application,
+                v$,
             };
         },
-        //validations() {
-        //    return {
-        //        applicationData: {
-        //            name: { required },
-        //            email: { required, email },
-        //            phone: { required, number },
-        //        }
-        //    };
-        //},
-        setup() {
-            return { v$: useVuelidate() };
-        },
+
         methods: {
             onSendApplication() {
-                return axios.post('api/Applications', {
-                    name: this.applicationData.name,
-                    email: this.applicationData.email,
-                    phone: this.applicationData.phone,
-                });
+                this.v$.$touch(); 
+                try {
+                    return axios.post('api/Applications', {
+                        name: this.application.name,
+                        email: this.application.email,
+                        phone: this.application.phone,
+                    });
+                } catch (error) {
+                    console.error(error);
+                }
             }
         }
     };
 </script>
-
