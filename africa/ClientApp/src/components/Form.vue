8b31f3c4c3f572029c188288ef6920c71440e9e0
lizau <liza.uchaneva@outlook.com>
2024-06-26 20:54:12 -0700

    Added popup message

--- africa/ClientApp/src/components/Form.vue
+++ africa/ClientApp/src/components/Form.vue
@@ -7,95 +7,114 @@
         <div class="form__input_box">
             <label class="form__label">Name</label>
             <input class="form__input"
-                   v-bind:class="{ 'form__input_hasError': v$.name.$invalid }"
-                   v-model="v$.name.$model"
+                   :class="{ 'form__input_hasError': (v$.name.$invalid && v$.name.$dirty) }"
+                   v-model="application.name"
                    type="text"
-                   name="name"
-                   id="" />
+                   name="name" />
             <p class="form__error_message"
-                  v-for="err in v$.name.$errors"
-                  :key="err.$uid">
+               v-for="err in v$.name.$errors"
+               :key="err.$uid">
                 *{{ err.$message }}
             </p>
         </div>
         <div class="form__input_box">
             <label class="form__label">Phone</label>
-            <input v-model="v$.phone.$model"
-                   v-bind:class="{ 'form__input_hasError': v$.phone.$invalid }"
+            <input v-model="application.phoneNumber"
+                   :class="{ 'form__input_hasError': (v$.phoneNumber.$invalid && v$.phoneNumber.$dirty) }"
                    class="form__input"
                    type="tel"
-                   name="phone"
-                   id="" />
+                   name="phoneNumber" />
             <p class="form__error_message"
-                  v-for="err in v$.phone.$errors"
-                  :key="err.$uid">
+               v-for="err in v$.phoneNumber.$errors"
+               :key="err.$uid">
                 *{{ err.$message }}
             </p>
         </div>
         <div class="form__input_box">
             <label class="form__label">Email</label>
-            <input v-model.trim="v$.email.$model"
-                   v-bind:class="{ 'form__input_hasError': v$.email.$invalid }"
+            <input v-model.trim="application.email"
+                   :class="{ 'form__input_hasError': (v$.email.$invalid && v$.email.$dirty) }"
                    class="form__input"
-                   type="email"
-                   name=""
-                   id="" />
+                   type="text"
+                   name="email" />
             <p class="form__error_message"
-                  v-for="err in v$.email.$errors"
-                  :key="err.$uid">
+               v-for="err in v$.email.$errors"
+               :key="err.$uid">
                 *{{ err.$message }}
             </p>
         </div>
         <button type="submit" class="form__button">Send</button>
     </form>
+    <transition name ="popup__fade" appear>
+        <div class="popup" v-if="visible.value">
+            <div class="popup__inner">
+                <p class="popup__title">Thank you for contacting us!</p>
+                <p class="popup__text">
+                        We have received your contact information<br />
+                        and will reach out to you as soon as possible
+                </p>
+                <button class="popup__button" @click="visible.value = false">Close</button>
+            </div>
+        </div>
+    </transition>
 </template>
 
 <script>
     import axios from 'axios';
     import { reactive, computed } from 'vue';
-    import { email, required, helpers } from '@vuelidate/validators';
+    import { email, required } from '@vuelidate/validators';
     import { useVuelidate } from '@vuelidate/core';
 
     export default {
         setup() {
             const application = reactive({
                 name: '',
-                phone: '',
+                phoneNumber: '',
                 email: '',
             });
 
-            const number = helpers.regex(
-                "serial",
-                /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/
-            );
-
             const rules = computed(() => ({
                 name: { required },
-                email: { required, email },
-                phone: { required, number }
+                phoneNumber: { required },
+                email: { required, email }
             }));
 
             const v$ = useVuelidate(rules, application);
 
+            const visible = reactive({ value: false });
+
+            const resetForm = () => {
+                application.name = '';
+                application.phoneNumber = '';
+                application.email = '';
+            };
+
             return {
                 application,
                 v$,
+                visible,
+                resetForm,
             };
         },
 
         methods: {
-            onSendApplication() {
-                this.v$.$touch(); 
-                try {
-                    return axios.post('api/Applications', {
-                        name: this.application.name,
-                        email: this.application.email,
-                        phone: this.application.phone,
-                    });
-                } catch (error) {
-                    console.error(error);
+            async onSendApplication() {
+                this.v$.$touch();
+                if (!this.v$.$invalid) {
+                    try {
+                        await axios.post('api/Applications', {
+                            name: this.application.name,
+                            phoneNumber: this.application.phoneNumber,
+                            email: this.application.email,
+                        });
+                    } catch (error) {
+                        console.error('Error submitting application:', error.response);
+                    }
+                    this.visible.value = true;
+                    this.resetForm();
+                    this.v$.$reset();
                 }
-            }
+            },
         }
     };
 </script>
